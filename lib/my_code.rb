08f0e05112ed65e30f770457f9710032b6724000
Ian Hutson <ih00209@georgiasouthern.edu>
2020-01-16 19:02:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -42,11 +42,26 @@ def reduce_to_total(source_array, starting_point)
   new = starting_point
   i = 0
   while i < source_array.length do
-    return false if !source_array[i]
+    new += source_array[i]
     i += 1
   end
   return true
 end
 
 def reduce_to_all_true(source_array)
+  i = 0
+  while i < source_array.length do
+    return false if !source_array[i]
+    i += 1
+  end
+  return true
+end
+
 def reduce_to_any_true(source_array)
+  i = 0
+  while i < source_array.length do
+    return false if source_array[i]
+    i += 1
+  end
+  return false
+end
