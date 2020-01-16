94333b19d5037434c2381aacf14bad23580bbf61
Ian Hutson <ih00209@georgiasouthern.edu>
2020-01-16 18:58:55 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -39,6 +39,8 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point)
-  
+  new = starting_point
+  i = 0
+  while i < source_array
 def reduce_to_all_true(source_array)
 def reduce_to_any_true(source_array)
